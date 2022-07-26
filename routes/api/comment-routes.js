1227a47f8617cc1dbc8c89bee78f726e49e1cdb0
Leonardo <josue.aguilar1995@gmail.com>
2022-07-25 19:06:47 -0500

    added reply function through controllers and front end

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 
 // add a comment route to pizza path
 router.route("/:pizzaId").post(addComment);
 
 // delete a comment and remove from a pizza path
-router.route("/:pizzaId/:commentId").delete(removeComment);
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 module.exports = router;
